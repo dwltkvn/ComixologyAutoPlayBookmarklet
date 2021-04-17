10382d3a9a5c063ce0d37c9bd31527a37fc693db
Dwlt Kvn <7428297+dwltkvn@users.noreply.github.com>
2021-04-17 23:35:28 +0200

    Check that current href is comixology
    href contains comixolog. and /comic-reader/

--- comixologyAutoplay.js
+++ comixologyAutoplay.js
@@ -1,38 +1,38 @@
-var delayString = prompt("Enter delay in second to change page" , "5");
-var delayInteger = parseInt(delayString) * 1000;
+if( window.location.href.includes('comixology.') === true && window.location.href.includes('/comic-reader/') === true )
+{
+	var delayString = prompt("Enter delay in second to change page" , "5");
+	var delayInteger = parseInt(delayString) * 1000;
 
-if (document.querySelector("#autoplay-btn")){
-	document.querySelector("#autoplay-btn").remove();
-	if (typeof window.autoplayInterval !== 'undefined') {
-		clearInterval(window.autoplayInterval);
+	if (document.querySelector("#autoplay-btn")){
+		document.querySelector("#autoplay-btn").remove();
+		if (typeof window.autoplayInterval !== 'undefined') {
+			clearInterval(window.autoplayInterval);
+		}
 	}
+
+	var sidebar = document.querySelector("nav#toolbar");
+	var refBtn = document.querySelector("#settings-btn");
+	var cloneBtn = refBtn.cloneNode(true);
+	cloneBtn.querySelector("h3").textContent = "Autoplay";
+	cloneBtn.querySelector("img").src = "https://m.media-amazon.com/images/G/01/Comics/web/i/assets/imgs/reader/rightnav.svg";
+	cloneBtn.id="autoplay-btn";
+	sidebar.appendChild(cloneBtn);
+	var state = 0;
+
+	cloneBtn.onclick = function(){
+		if(state===0)
+		{
+			cloneBtn.querySelector("img").src = "https://m.media-amazon.com/images/G/01/Comics/web/i/assets/imgs/reader/exit.svg";
+			console.log('Comixology Autoplay: ON');
+			state = 1;
+			window.autoplayInterval = setInterval(function(){ document.querySelector("button.right").click()} , delayInteger);
+		}
+		else
+		{
+			cloneBtn.querySelector("img").src = "https://m.media-amazon.com/images/G/01/Comics/web/i/assets/imgs/reader/rightnav.svg";
+			console.log('Comixology Autoplay: OFF');
+			state = 0;
+			clearInterval(window.autoplayInterval);
+		}
+	};
 }
-
-
-
-var sidebar = document.querySelector("nav#toolbar");
-var refBtn = document.querySelector("#settings-btn");
-var cloneBtn = refBtn.cloneNode(true);
-cloneBtn.querySelector("h3").textContent = "Autoplay";
-cloneBtn.querySelector("img").src = "https://m.media-amazon.com/images/G/01/Comics/web/i/assets/imgs/reader/rightnav.svg";
-cloneBtn.id="autoplay-btn";
-sidebar.appendChild(cloneBtn);
-var state = 0;
-
-cloneBtn.onclick = function(){
-	if(state===0)
-	{
-		cloneBtn.querySelector("img").src = "https://m.media-amazon.com/images/G/01/Comics/web/i/assets/imgs/reader/exit.svg";
-		console.log('Comixology Autoplay: ON');
-		state = 1;
-		window.autoplayInterval = setInterval(function(){ document.querySelector("button.right").click()} , delayInteger);
-	}
-	else
-	{
-		cloneBtn.querySelector("img").src = "https://m.media-amazon.com/images/G/01/Comics/web/i/assets/imgs/reader/rightnav.svg";
-		console.log('Comixology Autoplay: OFF');
-		state = 0;
-		clearInterval(window.autoplayInterval);
-	}
-};
-
